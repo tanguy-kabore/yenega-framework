27e8cedd27c1605c73a534a88233c393d263eea3
baowendnere <tanguykabore@yahoo.fr>
2024-02-08 23:12:22 +0100

    First commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from screen.auth.login import LoginScreen
+# from screen.auth.login import LoginScreen
 from screen.home import HomeScreen
+from yenega.screen.auth.login import LoginScreen
 if __name__ == "__main__":
-    HomeScreen('admin')
+    # HomeScreen('admin')
+    LoginScreen()
